055cd382cf31a7aab121dc6d029ba003bd21ce07
superBro <delocksuperbro@gmail.com>
2019-12-27 22:21:17 +0700

    delaetsa

--- PPL7/PPL7.cpp
+++ PPL7/PPL7.cpp
@@ -12,27 +12,39 @@
 
 using namespace std;
 
+/*
 typedef struct
 {
 	float x;
 	float y;
-} Point;
+} Point;*/
+
+typedef struct
+{
+	int v;
+	float m;
+	int pj;
+} minResp;
+
+const int OPER_UPDATE = 1;
+const int OPER_BUILD = 2;
+const int OPER_MIN = 3;
+const int OPER_EXIT = 4;
+const int OPER_INIT = 5;
 
 void readGraph();
 void writeMST();
+void initWorkers();
 void release();
 void buildMST();
 void runBuildKey(bool* key, bool* mstSet);
 void runMinKey(bool* key, bool* mstSet);
 void buildKey(bool* key, bool* mstSet, int pfrom, int count);
 void minKey(int& u, float& m, int& pj, bool* key, bool* mstSet, int pfrom, int count);
-void writeLog();
+//void writeLog();
 
-const int REPEAT_COUNT = 10;
-const int THREAD_COUNT = 3;
-int* timeMeter;
-thread threads[THREAD_COUNT];
-
+int procCount = 3;
+int curRank;
 int n; // vertex count
 int pathCount;
 float* graph;
@@ -40,37 +52,89 @@ int* result;
 int* minKeys;
 int* jj;
 float* mm;
-Point* points;
+//Point* points;
+bool* key;
+bool* mstSet;
+int* partsSize;
+int* starts;
+int partFrom;
+int partSize;
+
+void worker();
 
 int main(int argc, char* argv[])
 {
 	int ierr;
 	ierr = MPI_Init(&argc, &argv);
-	if (ierr != MPI_SUCCESS)
-		return ierr;
-
-	readGraph();
-	timeMeter = new int[REPEAT_COUNT];
-	minKeys = new int[THREAD_COUNT];
-	mm = new float[THREAD_COUNT];
-	jj = new int[THREAD_COUNT];
-	result = new int[n];
-	chrono::time_point<chrono::system_clock> start, end;
-	for (int w = 0; w < REPEAT_COUNT; w++)
-	{
-		start = chrono::system_clock::now();
+	if (ierr != MPI_SUCCESS) return ierr;
+	MPI_Comm_rank(MPI_COMM_WORLD, &curRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &procCount);
+	if (curRank == 0) {
+		readGraph();
+		minKeys = new int[procCount];
+		mm = new float[procCount];
+		jj = new int[procCount];
+		key = new bool[n];
+		mstSet = new bool[n];
+		result = new int[n];
+		initWorkers();
 		buildMST();
-		end = chrono::system_clock::now();
-		timeMeter[w] = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-		cout << "Round " << w + 1 << " finished" << endl;
+		writeMST();
+		release();
 	}
-	writeMST();
-	writeLog();
-	release();
-
+	else {
+		worker();
+	}
 	MPI_Finalize();
 }
 
+void initWorkers() {
+	int nominalCount = n / (procCount - 1);
+	partsSize = new int[procCount];
+	starts = new int[procCount];
+	for (int startIndex = 0, i = 1; startIndex < n; startIndex += nominalCount, i++) {
+		int count = nominalCount;
+		if (startIndex + nominalCount < n && startIndex + nominalCount * 2 >= n) {
+			count = n - startIndex;
+		}
+		partsSize[i] = count;
+		starts[i]] = startIndex;
+	}
+	for (int i = 1; i < procCount; i++) {
+		int oper = OPER_INIT;
+		MPI_Send(&oper, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&starts[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&partsSize[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(graph, n * n, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+		key = new bool[n];
+		mstSet = new bool[n];
+	}
+}
+
+void worker() {
+	int oper;
+	MPI_Status status;
+	MPI_Recv(&oper, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+	switch (oper) {
+	case OPER_INIT:
+		MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&partFrom, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(&partSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		graph = new float[n * n];
+		MPI_Recv(graph, n * n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+		break;
+	case OPER_EXIT:
+		return;
+		break;
+	case OPER_BUILD:
+		break;
+	case OPER_MIN:
+		break;
+	case OPER_UPDATE:
+		break;
+	}
+}
 
 void readGraph()
 {
@@ -85,8 +149,8 @@ void readGraph()
 	pathCount = (n * (n - 1)) / 2;
 	float* paths = new float[pathCount];
 	inf.read((char*)paths, pathCount * sizeof(float));
-	points = new Point[n];
-	inf.read((char*)points, n * sizeof(Point));
+	//points = new Point[n];
+	//inf.read((char*)points, n * sizeof(Point));
 	inf.close();
 	graph = new float[n * n];
 	int p = 0;
@@ -109,8 +173,6 @@ void readGraph()
 
 void buildMST()
 {
-	bool* key = new bool[n];
-	bool* mstSet = new bool[n];
 	for (int i = 0; i < n; i++)
 	{
 		key[i] = false;
@@ -126,7 +188,7 @@ void buildMST()
 		int u = 0;
 		int ji = 0;
 		float mmKey = FLT_MAX;
-		for (int j = 0; j < THREAD_COUNT; j++) {
+		for (int j = 0; j < procCount; j++) {
 			if (mm[j] < mmKey) {
 				u = minKeys[j];
 				mmKey = mm[j];
@@ -142,49 +204,59 @@ void buildMST()
 
 void runBuildKey(bool* key, bool* mstSet)
 {
-	int nominalCount = n / THREAD_COUNT;
-	for (int startIndex = 0, i = 0; startIndex < n; startIndex += nominalCount, i++)
+
+	//int oStartIndex = startIndex;
+	//int oCount = count;
+	MPI_Send(&oper, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+	//MPI_Send(&oStartIndex, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+	//MPI_Send(&oCount, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+	//MPI_Send(&key[oStartIndex], count, MPI_C_BOOL, i, 0, MPI_COMM_WORLD);
+	//MPI_Send(&mstSet[oStartIndex], count, MPI_C_BOOL, i, 0, MPI_COMM_WORLD);
+	//threads[i] = thread(buildKey, key, mstSet, startIndex, count);
+	if (count != nominalCount)
 	{
-		int count = nominalCount;
-		if (startIndex + nominalCount < n && startIndex + nominalCount * 2 >= n)
-		{
-			count = n - startIndex;
-		}
-		threads[i] = thread(buildKey, key, mstSet, startIndex, count);
-		if (count != nominalCount)
-		{
-			break;
-		}
+		break;
 	}
-	for (int i = 0; i < THREAD_COUNT; i++)
+
+	for (int i = 1; i < procCount; i++)
 	{
-		threads[i].join();
+		int t;
+		MPI_Status status;
+		MPI_Recv(&t, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
 	}
 }
 
 void runMinKey(bool* key, bool* mstSet)
 {
-	int nominalCount = n / THREAD_COUNT;
-	for (int startIndex = 0, i = 0; startIndex < n; startIndex += nominalCount, i++)
+	int nominalCount = n / (procCount - 1);
+	for (int startIndex = 0, i = 1; startIndex < n; startIndex += nominalCount, i++)
 	{
 		int count = nominalCount;
 		if (startIndex + nominalCount < n && startIndex + nominalCount * 2 >= n)
 		{
 			count = n - startIndex;
 		}
-		threads[i] = thread(minKey, ref(minKeys[i]), ref(mm[i]), ref(jj[i]), key, mstSet, startIndex, count);
+		int oper = OPER_MIN;
+		int oStartIndex = startIndex;
+		int oCount = count;
+		MPI_Send(&oper, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&oStartIndex, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&oCount, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&key[oStartIndex], count, MPI_C_BOOL, i, 0, MPI_COMM_WORLD);
+		MPI_Send(&mstSet[oStartIndex], count, MPI_C_BOOL, i, 0, MPI_COMM_WORLD);
+		//threads[i] = thread(minKey, ref(minKeys[i]), ref(mm[i]), ref(jj[i]), key, mstSet, startIndex, count);
 		if (count != nominalCount)
 		{
 			break;
 		}
 	}
-	for (int i = 0; i < THREAD_COUNT; i++)
+	for (int i = 0; i < procCount; i++)
 	{
-		threads[i].join();
+		//threads[i].join();
 	}
 }
 
-void buildKey(bool* key, bool* mstSet, int pfrom, int count)
+void buildKey(int pfrom, int count)
 {
 	int k = 0;
 	for (int i = pfrom; k < count; i++, k++)
@@ -213,7 +285,7 @@ void buildKey(bool* key, bool* mstSet, int pfrom, int count)
 	}
 }
 
-void minKey(int& v, float& m, int& pj, bool* key, bool* mstSet, int pfrom, int count)
+minResp minKey(int pfrom, int count)
 {
 	float min = FLT_MAX;
 	int minIndex = 0;
@@ -238,9 +310,10 @@ void minKey(int& v, float& m, int& pj, bool* key, bool* mstSet, int pfrom, int c
 			}
 		}
 	}
-	v = minIndex;
-	m = min;
-	pj = jetIndex;
+	minResp result;
+	result.v = minIndex;
+	result.m = min;
+	result.pj = jetIndex;
 }
 
 void writeMST()
@@ -257,55 +330,15 @@ void writeMST()
 	outf.close();
 }
 
-void writeLog()
-{
-	cout << "Writing time log ..." << endl;
-	ofstream outf("log/time_par.txt", ios::out);
-	if (!outf)
-	{
-		cerr << "Time log writing error!" << endl;
-		exit(1);
-	}
-	int timeMin, timeMax, timeSum;
-	timeMin = timeMeter[0];
-	timeMax = timeMeter[0];
-	timeSum = 0 + timeMeter[0];
-	float timeAverage;
-	for (int i = 1; i < REPEAT_COUNT; i++)
-	{
-		int t = timeMeter[i];
-		if (t < timeMin)
-		{
-			timeMin = t;
-		}
-		if (t > timeMax)
-		{
-			timeMax = t;
-		}
-		timeSum += t;
-	}
-	timeAverage = ((float)timeSum) / REPEAT_COUNT;
-	outf << "Time test \"Parallel\"" << endl;
-	outf << "Threads: " << THREAD_COUNT << endl;
-	outf << "Repeat: " << REPEAT_COUNT << endl;
-	outf << "Time Average (ms): " << timeAverage << endl;
-	outf << "Time Min (ms): " << timeMin << endl;
-	outf << "Time Max (ms): " << timeMax << endl;
-	for (int i = 0; i < REPEAT_COUNT; i++)
-	{
-		outf << "[" << i << "] " << timeMeter[i] << endl;
-	}
-	outf.close();
-}
-
 void release()
 {
 	delete[] graph;
 	delete[] result;
 	delete[] points;
-	delete[] timeMeter;
 	delete[] minKeys;
 	delete[] mm;
 	delete[] jj;
+	delete[] key;
+	delete[] mstSet;
 	cout << "Completed" << endl;
 }
